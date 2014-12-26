0f5de2e2bfa776f2a909ab6b0a2f54ec939b6fb1
Luis Rovirosa <luisrovirosa@gmail.com>
2014-12-26 17:19:32 +0100

    Refactoring tests: move assertions to tearDown

--- php/test/gilded_rose_test.php
+++ php/test/gilded_rose_test.php
@@ -10,26 +10,40 @@ class GildedRoseTest extends PHPUnit_Framework_TestCase
     const SELL_IN_EXPIRED_DATE = -10;
     const NORMAL_QUALITY = 100;
 
+    private $items;
+    private $expectedQuality;
+    private $expectedDate;
+    private $expectedProductName;
+
+    protected function setUp()
+    {
+        $this->expectedQuality = self::NORMAL_QUALITY - 1;
+        $this->expectedDate = self::SELL_IN_POSITIVE_DAYS - 1;
+        $this->expectedProductName = self::NORMAL_PRODUCT;
+    }
+
     /** @test  */
     function shouldDecreaseTheQualityBy1WhenNormalProductHasValue()
     {
-        $items = array(new Item(self::NORMAL_PRODUCT, self::SELL_IN_POSITIVE_DAYS, self::NORMAL_QUALITY));
-        $gildedRose = new GildedRose($items);
-        $gildedRose->update_quality();
-        $this->assertEquals(self::NORMAL_QUALITY - 1, $items[0]->quality);
-        $this->assertEquals(self::SELL_IN_POSITIVE_DAYS - 1, $items[0]->sell_in);
-        $this->assertEquals(self::NORMAL_PRODUCT, $items[0]->name);
+        $this->items = array(new Item(self::NORMAL_PRODUCT, self::SELL_IN_POSITIVE_DAYS, self::NORMAL_QUALITY));
     }
 
     /** @test */
     public function shouldDecreaseTheQualityBy2WhenNormalProductHasValueAndIsNotExpired()
     {
-        $items = array(new Item(self::NORMAL_PRODUCT, self::SELL_IN_EXPIRED_DATE, self::NORMAL_QUALITY));
-        $gildedRose = new GildedRose($items);
+        $this->items = array(new Item(self::NORMAL_PRODUCT, self::SELL_IN_EXPIRED_DATE, self::NORMAL_QUALITY));
+        $this->expectedQuality = self::NORMAL_QUALITY - 2;
+        $this->expectedDate = self::SELL_IN_EXPIRED_DATE - 1;
+    }
+
+    protected function tearDown()
+    {
+        $gildedRose = new GildedRose($this->items);
         $gildedRose->update_quality();
-        $this->assertEquals(self::NORMAL_QUALITY - 2, $items[0]->quality);
-        $this->assertEquals(self::SELL_IN_EXPIRED_DATE - 1, $items[0]->sell_in);
-        $this->assertEquals(self::NORMAL_PRODUCT, $items[0]->name);
+        $item = $this->items[0];
+        $this->assertEquals($this->expectedQuality, $item->quality);
+        $this->assertEquals($this->expectedDate, $item->sell_in);
+        $this->assertEquals($this->expectedProductName, $item->name);
     }
 
 }
