8ffff8336eafaa131fd9e4d1af6fc25cab672dc2
Luis Rovirosa <luisrovirosa@gmail.com>
2014-12-26 17:25:09 +0100

    Test refactor

--- php/test/gilded_rose_test.php
+++ php/test/gilded_rose_test.php
@@ -11,7 +11,7 @@ class GildedRoseTest extends PHPUnit_Framework_TestCase
     const NORMAL_QUALITY = 100;
     const ZERO_QUALITY = 0;
 
-    private $items;
+    private $item;
     private $expectedQuality;
     private $expectedDate;
     private $expectedProductName;
@@ -26,13 +26,13 @@ class GildedRoseTest extends PHPUnit_Framework_TestCase
     /** @test  */
     function shouldDecreaseTheQualityBy1WhenNormalProductHasValue()
     {
-        $this->items = array(new Item(self::NORMAL_PRODUCT, self::SELL_IN_POSITIVE_DAYS, self::NORMAL_QUALITY));
+        $this->item = new Item(self::NORMAL_PRODUCT, self::SELL_IN_POSITIVE_DAYS, self::NORMAL_QUALITY);
     }
 
     /** @test */
     public function shouldDecreaseTheQualityBy2WhenNormalProductHasValueAndIsNotExpired()
     {
-        $this->items = array(new Item(self::NORMAL_PRODUCT, self::SELL_IN_EXPIRED_DATE, self::NORMAL_QUALITY));
+        $this->item = new Item(self::NORMAL_PRODUCT, self::SELL_IN_EXPIRED_DATE, self::NORMAL_QUALITY);
         $this->expectedQuality = self::NORMAL_QUALITY - 2;
         $this->expectedDate = self::SELL_IN_EXPIRED_DATE - 1;
     }
@@ -40,26 +40,25 @@ class GildedRoseTest extends PHPUnit_Framework_TestCase
     /** @test */
     public function shouldBe0WhenTheQualityIs0()
     {
-        $this->items = array(new Item(self::NORMAL_PRODUCT, self::SELL_IN_POSITIVE_DAYS, self::ZERO_QUALITY));
+        $this->item = new Item(self::NORMAL_PRODUCT, self::SELL_IN_POSITIVE_DAYS, self::ZERO_QUALITY);
         $this->expectedQuality = self::ZERO_QUALITY;
     }
 
     /** @test */
     public function shouldBe0WhenTheQualityIs0EvenIfIsExpired()
     {
-        $this->items = array(new Item(self::NORMAL_PRODUCT, self::SELL_IN_EXPIRED_DATE, self::ZERO_QUALITY));
+        $this->item = new Item(self::NORMAL_PRODUCT, self::SELL_IN_EXPIRED_DATE, self::ZERO_QUALITY);
         $this->expectedQuality = self::ZERO_QUALITY;
         $this->expectedDate = self::SELL_IN_EXPIRED_DATE - 1;
     }
 
     protected function tearDown()
     {
-        $gildedRose = new GildedRose($this->items);
+        $gildedRose = new GildedRose(array($this->item));
         $gildedRose->update_quality();
-        $item = $this->items[0];
-        $this->assertEquals($this->expectedQuality, $item->quality);
-        $this->assertEquals($this->expectedDate, $item->sell_in);
-        $this->assertEquals($this->expectedProductName, $item->name);
+        $this->assertEquals($this->expectedQuality, $this->item->quality);
+        $this->assertEquals($this->expectedDate, $this->item->sell_in);
+        $this->assertEquals($this->expectedProductName, $this->item->name);
     }
 
 }
