51ebcdfe2052de7bd6c1f1c25e119f89e9fd5d1e
Luis Rovirosa <luisrovirosa@gmail.com>
2014-12-26 17:12:09 +0100

    Test quality decrease by 2 when the date is expired

--- php/test/gilded_rose_test.php
+++ php/test/gilded_rose_test.php
@@ -2,18 +2,34 @@
 
 require_once 'gilded_rose.php';
 
-class GildedRoseTest extends PHPUnit_Framework_TestCase {
+class GildedRoseTest extends PHPUnit_Framework_TestCase
+{
+
     const NORMAL_PRODUCT = "foo";
     const SELL_IN_POSITIVE_DAYS = 10;
+    const SELL_IN_EXPIRED_DATE = -10;
+    const NORMAL_QUALITY = 100;
 
     /** @test  */
-    function shouldDecreaseTheQualityWhenNormalProductHasValue() {
-        $quality = 100;
-        $items = array(new Item(self::NORMAL_PRODUCT, self::SELL_IN_POSITIVE_DAYS, $quality));
+    function shouldDecreaseTheQualityBy1WhenNormalProductHasValue()
+    {
+        $items = array(new Item(self::NORMAL_PRODUCT, self::SELL_IN_POSITIVE_DAYS, self::NORMAL_QUALITY));
         $gildedRose = new GildedRose($items);
         $gildedRose->update_quality();
-        $this->assertEquals($quality-1, $items[0]->quality);
-        $this->assertEquals(self::SELL_IN_POSITIVE_DAYS-1, $items[0]->sell_in);
+        $this->assertEquals(self::NORMAL_QUALITY - 1, $items[0]->quality);
+        $this->assertEquals(self::SELL_IN_POSITIVE_DAYS - 1, $items[0]->sell_in);
         $this->assertEquals(self::NORMAL_PRODUCT, $items[0]->name);
     }
+
+    /** @test */
+    public function shouldDecreaseTheQualityBy2WhenNormalProductHasValueAndIsNotExpired()
+    {
+        $items = array(new Item(self::NORMAL_PRODUCT, self::SELL_IN_EXPIRED_DATE, self::NORMAL_QUALITY));
+        $gildedRose = new GildedRose($items);
+        $gildedRose->update_quality();
+        $this->assertEquals(self::NORMAL_QUALITY - 2, $items[0]->quality);
+        $this->assertEquals(self::SELL_IN_EXPIRED_DATE - 1, $items[0]->sell_in);
+        $this->assertEquals(self::NORMAL_PRODUCT, $items[0]->name);
+    }
+
 }
