6dbf91385f05bb413ed178d83a29b00111e3ce5f
Luis Rovirosa <luisrovirosa@gmail.com>
2014-12-26 19:36:44 +0100

    Assert minimum and maximum value

--- php/src/Rules/BaseRule.php
+++ php/src/Rules/BaseRule.php
@@ -23,7 +23,19 @@ abstract class BaseRule
 
     protected function updateQuality($item)
     {
-        $item->quality = max(0, min(50, $item->quality + $this->getQualityIncrement($item)));
+        $item->quality = $item->quality + $this->getQualityIncrement($item);
+        $this->assertMinimumValue($item);
+        $this->assertMaximumValue($item);
+    }
+
+    private function assertMinimumValue($item)
+    {
+        $item->quality = max(0, $item->quality);
+    }
+
+    private function assertMaximumValue($item)
+    {
+        $item->quality = min(50, $item->quality);
     }
 
     abstract public function match($item);
