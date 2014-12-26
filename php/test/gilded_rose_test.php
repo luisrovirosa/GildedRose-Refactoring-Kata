7b14c37f3f3f2b07e6b7a439da8d1f11de44547a
Luis Rovirosa <luisrovirosa@gmail.com>
2014-12-26 11:04:28 +0100

    First PHP test

--- php/test/gilded_rose_test.php
+++ php/test/gilded_rose_test.php
@@ -4,11 +4,13 @@ require_once 'gilded_rose.php';
 
 class GildedRoseTest extends PHPUnit_Framework_TestCase {
 
-    function testFoo() {
-        $items = array(new Item("foo", 0, 0));
+    /** @test  */
+    function shouldDecreaseTheQualityWhenNormalProductHasValue() {
+        $quality = 100;
+        $items = array(new Item("foo", 10, $quality));
         $gildedRose = new GildedRose($items);
         $gildedRose->update_quality();
-        $this->assertEquals("fixme", $items[0]->name);
+        $this->assertEquals($quality-1, $items[0]->quality);
     }
 
 }
