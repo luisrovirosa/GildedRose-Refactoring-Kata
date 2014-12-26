b88443cdf9a4938c50607000f05968a73ac9cea6
Luis Rovirosa <luisrovirosa@gmail.com>
2014-12-26 11:10:56 +0100

    Added verification that the name doesn't change and the sell_in value is decreased by 1

--- php/test/gilded_rose_test.php
+++ php/test/gilded_rose_test.php
@@ -3,14 +3,17 @@
 require_once 'gilded_rose.php';
 
 class GildedRoseTest extends PHPUnit_Framework_TestCase {
+    const NORMAL_PRODUCT = "foo";
+    const SELL_IN_POSITIVE_DAYS = 10;
 
     /** @test  */
     function shouldDecreaseTheQualityWhenNormalProductHasValue() {
         $quality = 100;
-        $items = array(new Item("foo", 10, $quality));
+        $items = array(new Item(self::NORMAL_PRODUCT, self::SELL_IN_POSITIVE_DAYS, $quality));
         $gildedRose = new GildedRose($items);
         $gildedRose->update_quality();
         $this->assertEquals($quality-1, $items[0]->quality);
+        $this->assertEquals(self::SELL_IN_POSITIVE_DAYS-1, $items[0]->sell_in);
+        $this->assertEquals(self::NORMAL_PRODUCT, $items[0]->name);
     }
-
 }
