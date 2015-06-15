814ae42261b7dd47e6113fa448348d95b8676a06
Luis Rovirosa <luisrovirosa@gmail.com>
2015-06-15 07:42:35 +0200

    sell_in_decreases_every_day

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,31 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class GildedRoseTest {
 
-    @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+	// Every day:
+	// - Sell in decreases every day
+	@Test
+	public void sell_in_decreases_every_day() {
+		Item[] items = new Item[] { new Item("foo", 10, 5) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals(9, app.items[0].sellIn);
+	}
+	// - Quality decreases every day
+
+	// - Once the sell by date has passed, Quality degrades twice as fast
+	// - The Quality of an item is never negative
+	// - "Aged Brie" actually increases in Quality the older it gets
+	// - The Quality of an item is never more than 50
+	// - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+	// - "Backstage passes",
+	// - Increases in Quality as it's SellIn value approaches
+	// - Quality increases by 2 when there are 10 days or less
+	// - By 3 when there are 5 days or less but
+	// - Quality drops to 0 after the concert
 
 }
