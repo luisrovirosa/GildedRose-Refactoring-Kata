49ae3068ed9d308a9e85933caf26cc19c8a97a48
Luis Rovirosa <luisrovirosa@gmail.com>
2015-06-15 07:50:19 +0200

    refactor: extract method

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -11,10 +11,30 @@ public class GildedRoseTest {
 	@Test
 	public void sell_in_decreases_every_day() {
 		GildedRose app = gildedRose(normalItem());
-		app.updateQuality();
 		int expectedSellIn = 9;
-		assertEquals(expectedSellIn, app.items[0].sellIn);
+		app.updateQuality();
+		assertSellIn(app, expectedSellIn);
 	}
+
+	// - Quality decreases every day
+	@Test
+	public void quality_decreases_every_day() {
+		Item[] items = new Item[] { new Item("foo", 10, 5) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals(4, app.items[0].quality);
+	}
+
+	// - Once the sell by date has passed, Quality degrades twice as fast
+	// - The Quality of an item is never negative
+	// - "Aged Brie" actually increases in Quality the older it gets
+	// - The Quality of an item is never more than 50
+	// - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+	// - "Backstage passes",
+	// - Increases in Quality as it's SellIn value approaches
+	// - Quality increases by 2 when there are 10 days or less
+	// - By 3 when there are 5 days or less but
+	// - Quality drops to 0 after the concert
 
 	private GildedRose gildedRose(Item item) {
 		Item[] items = new Item[] { item };
@@ -27,23 +47,8 @@ public class GildedRoseTest {
 		return item;
 	}
 
-	// - Quality decreases every day
-	@Test
-	public void quality_decreases_every_day() {
-		Item[] items = new Item[] { new Item("foo", 10, 5) };
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals(4, app.items[0].quality);
+	private void assertSellIn(GildedRose app, int expectedSellIn) {
+		assertEquals(expectedSellIn, app.items[0].sellIn);
 	}
-	// - Once the sell by date has passed, Quality degrades twice as fast
-	// - The Quality of an item is never negative
-	// - "Aged Brie" actually increases in Quality the older it gets
-	// - The Quality of an item is never more than 50
-	// - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
-	// - "Backstage passes",
-	// - Increases in Quality as it's SellIn value approaches
-	// - Quality increases by 2 when there are 10 days or less
-	// - By 3 when there are 5 days or less but
-	// - Quality drops to 0 after the concert
 
 }
