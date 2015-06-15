09f2551fb36ee52d7a7b6331890c3b91391a85fa
Luis Rovirosa <luisrovirosa@gmail.com>
2015-06-15 08:08:44 +0200

    quality_decreases_twice_as_fast_one_sell_in_has_passed

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -7,9 +7,10 @@ import org.junit.Test;
 public class GildedRoseTest {
 
 	private static final int DEFAULT_QUALITY_CHANGE = -1;
-	private static final int DEFAULT_SELL_IN_CHANGE = -1;
-
 	private static final int NORMAL_ITEM_QUALITY = 5;
+
+	private static final int DEFAULT_SELL_IN_CHANGE = -1;
+	private static final int EXPIRED_SELL_IN = 0;
 	private static final int NORMAL_ITEM_SELL_IN = 10;
 
 	@Test
@@ -22,17 +23,13 @@ public class GildedRoseTest {
 		verifyQuality(NORMAL_ITEM_QUALITY + DEFAULT_QUALITY_CHANGE, normalItem());
 	}
 
-	private void verifyQuality(int expectedQuality, Item normalItem) {
-		GildedRose gildedRose = gildedRose(normalItem);
-		gildedRose.updateQuality();
-		assertQuality(expectedQuality, gildedRose);
-	}
-
-	private void assertQuality(int expectedQuality, GildedRose gildedRose) {
-		assertEquals(expectedQuality, gildedRose.items[0].quality);
-	}
-
 	// - Once the sell by date has passed, Quality degrades twice as fast
+	@Test
+	public void quality_decreases_twice_as_fast_one_sell_in_has_passed() {
+		Item expiredItem = new Item("foo", EXPIRED_SELL_IN, NORMAL_ITEM_QUALITY);
+		verifyQuality(NORMAL_ITEM_QUALITY + 2 * DEFAULT_QUALITY_CHANGE, expiredItem);
+	}
+
 	// - The Quality of an item is never negative
 	// - "Aged Brie" actually increases in Quality the older it gets
 	// - The Quality of an item is never more than 50
@@ -49,6 +46,12 @@ public class GildedRoseTest {
 		assertSellIn(expectedSellIn, gildedRose);
 	}
 
+	private void verifyQuality(int expectedQuality, Item normalItem) {
+		GildedRose gildedRose = gildedRose(normalItem);
+		gildedRose.updateQuality();
+		assertQuality(expectedQuality, gildedRose);
+	}
+
 	private GildedRose gildedRose(Item item) {
 		Item[] items = new Item[] { item };
 		GildedRose app = new GildedRose(items);
@@ -62,6 +65,10 @@ public class GildedRoseTest {
 
 	private void assertSellIn(int expectedSellIn, GildedRose app) {
 		assertEquals(expectedSellIn, app.items[0].sellIn);
+	}
+
+	private void assertQuality(int expectedQuality, GildedRose gildedRose) {
+		assertEquals(expectedQuality, gildedRose.items[0].quality);
 	}
 
 }
