2841a2278facd6804d06c264c06130efb12021a0
Luis Rovirosa <luisrovirosa@gmail.com>
2015-06-15 09:54:40 +0200

    refactor: extract method

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -76,9 +76,13 @@ public class GildedRoseTest {
 	// - By 3 when there are 5 days or less but
 	@Test
 	public void backstage_increase_quality_by_3_when_sell_in_is_5_or_less() {
-		Item item = new Item(BACKSTAGE_NAME, 5, NORMAL_ITEM_QUALITY);
+		Item item = backstageIn5Days();
 
 		verifyQuality(NORMAL_ITEM_QUALITY + 3 * QUALITY_CHANGE, item);
+	}
+
+	private Item backstageIn5Days() {
+		return new Item(BACKSTAGE_NAME, 5, NORMAL_ITEM_QUALITY);
 	}
 
 	// - Quality drops to 0 after the concert
